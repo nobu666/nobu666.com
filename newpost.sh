e2be55dab192853d516fdad1b5c2c46a2a2005f5
nobu666 <ogata@gmail.com>
2024-02-01 23:34:46 +0900

    fixed script

--- newpost.sh
+++ newpost.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-new_seq=$(( $(basename $(find content -type f -name '*.md' | awk -F/ '{print $5}' | sort -nr | head -n1) .md) + 1 ))
-hugo new $(date +%Y/%m/%d)/${new_seq}.md | awk '{print $1}' | pbcopy
+new_seq=$(( $(basename $(find content -type f -name '*.md' | awk -F/ '{print $6}' | sort -nr | head -n1) .md) + 1 ))
+hugo new $(date +%Y/%m/%d)/${new_seq}.md | awk -F\" '{print $2}' | xsel --clipboard --input
